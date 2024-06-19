2abaf4d2e1e100ea1fb332b7047e6b63f100562a
Naol Chala <naolchala6@gmail.com>
2024-06-19 09:59:23 +0300

    Setup Eslint

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import js from "@eslint/js";
+export default [
+	js.configs.recommended,
+	{
+		rules: {
+			"no-unused-vars": "error",
+			"no-undef": "error",
+		},
+		files: ["**/*.ts", "**/*.cts", "**.*.mts"],
+	},
+];
